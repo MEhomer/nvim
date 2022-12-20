9cf5778343752306e80981952ae457e9244e6433
Dimitar Venov <venovdimitar@gmail.com>
2022-12-20 11:40:27 +0100

    Updates to `plugins/treesitter.lua`.

--- lua/mehomer/plugins-setup.lua
+++ lua/mehomer/plugins-setup.lua
@@ -1,4 +1,4 @@
--- auto install packer if not installed
+-- Auto install packer if not installed
 local ensure_packer = function()
 	local fn = vim.fn
 	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
